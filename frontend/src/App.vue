ab596906407b2ad2df0889a8eb21cb676da521b9
dsagi727 <dsagi727@gmail.com>
2024-08-31 15:59:08 +0200

    add tailwind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,3 @@
 <script setup></script>
 
 <template></template>
-
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
-
-:root {
-  --primary: #1a1a1a;
-  --action: #f63900;
-  --light-grey: #f6f6f6;
-}
-
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body {
-  font-family: Montserrat, sans-serif;
-  background: var(--primary);
-  color: #fff;
-}
-</style>

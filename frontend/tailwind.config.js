1b2bf8fa77905ba79438b78c5969745d68cc284e
dsagi727 <dsagi727@gmail.com>
2024-09-01 00:09:01 +0200

    add tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      custom: ['Montserrat', 'sans-serif'],
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      primary: 'var(--primary)',
+      secondary: 'var(--light-grey)',
+      action: 'var(--action)',
+    },
+    extend: {
+      transitionProperty: {
+        width: 'width',
+      },
+      width: {
+        hovered: 'calc(100% - 2px)',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      width: ['hover'],
+    },
   },
   plugins: [],
 }
-

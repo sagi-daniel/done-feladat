8535b96915f81d652095c11a5931e185e58ca661
dsagi727 <dsagi727@gmail.com>
2024-09-01 00:12:49 +0200

    add basic layout

---
+++ frontend/src/components/layout/AppLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex justify-between">
+    <Sidebar />
+    <Main>
+      <router-view />
+    </Main>
+  </div>
+</template>
+
+<script setup>
+import Sidebar from './Sidebar.vue'
+import Main from './Main.vue'
+</script>
---
+++ frontend/src/components/layout/Main.vue
@@ -0,0 +1,7 @@
+<template>
+  <main
+    class="flex w-full flex-col gap-10 justify-center items-center text-content rounded-tl-3xl rounded-bl-3xl bg-primary text-secondary"
+  >
+    <slot />
+  </main>
+</template>

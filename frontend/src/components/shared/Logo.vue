64216544c8f31755a75a8d825c66997eff990920
dsagi727 <dsagi727@gmail.com>
2024-09-01 00:13:15 +0200

    add components

---
+++ frontend/src/components/shared/Logo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :class="['flex', alignmentClasses[align], sizeClasses[size], 'h-auto']">
+    <router-link to="/">
+      <img :src="logoSrc" alt="logo" />
+    </router-link>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  align: {
+    type: String,
+    default: 'left',
+  },
+  size: {
+    type: String,
+    default: 'large',
+  },
+})
+
+const logoSrc = 'assets/brand/logo-l.svg'
+
+const alignmentClasses = {
+  left: 'justify-start',
+  center: 'justify-center',
+  right: 'justify-end',
+}
+
+const sizeClasses = {
+  xsmall: 'w-16',
+  small: 'w-20',
+  medium: 'w-32',
+  large: 'w-40',
+}
+</script>

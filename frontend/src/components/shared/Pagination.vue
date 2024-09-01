9ba66733e5bd7b8db0903d307f67c2ea1a736ec8
dsagi727 <dsagi727@gmail.com>
2024-09-01 14:20:59 +0200

    add pagination for later

---
+++ frontend/src/components/shared/Pagination.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { defineProps } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+const props = defineProps({
+  icon: {
+    type: Object,
+    required: true,
+  },
+  onClick: {
+    type: Function,
+    required: true,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  className: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<template>
+  <button :class="['px-4 py-2 rounded-2xl', className]" @click="onClick" :disabled="disabled">
+    <FontAwesomeIcon :icon="icon" />
+  </button>
+</template>

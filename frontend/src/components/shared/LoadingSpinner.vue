b91d4acf53bf0410f716aaa06ab566d8e8e469a0
dsagi727 <dsagi727@gmail.com>
2024-09-01 14:20:12 +0200

    faet:  add loadingspinner for later

---
+++ frontend/src/components/shared/LoadingSpinner.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <div class="w-12 h-12 border-4 border-action border-t-transparent border-solid rounded-full animate-spin"></div>
+  </div>
+</template>

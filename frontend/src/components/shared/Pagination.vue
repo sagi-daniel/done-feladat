f0781512d96b5ffe46098873b98477155f4bbf68
dsagi727 <dsagi727@gmail.com>
2024-09-01 21:02:54 +0200

    add pagination with debouncer prevent rapid state changes

--- frontend/src/components/shared/Pagination.vue
+++ frontend/src/components/shared/Pagination.vue
@@ -1,29 +1,47 @@
 <script setup>
-import { defineProps } from 'vue'
+import { defineProps, defineEmits, ref } from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { debounce } from '../../utils/helpers'
 
 const props = defineProps({
-  icon: {
-    type: Object,
+  currentPage: {
+    type: Number,
     required: true,
   },
-  onClick: {
-    type: Function,
+  totalPages: {
+    type: Number,
     required: true,
-  },
-  disabled: {
-    type: Boolean,
-    default: false,
-  },
-  className: {
-    type: String,
-    default: '',
   },
 })
+
+const emits = defineEmits(['page-change'])
+
+// Debounced changePage function to prevent rapid state changes
+const changePage = debounce(newPage => {
+  if (newPage >= 1 && newPage <= props.totalPages) {
+    emits('page-change', newPage)
+  }
+}, 300)
 </script>
 
 <template>
-  <button :class="['px-4 py-2 rounded-2xl', className]" @click="onClick" :disabled="disabled">
-    <FontAwesomeIcon :icon="icon" />
-  </button>
+  <div class="flex justify-start items-center space-x-2">
+    <button class="px-4 py-2 rounded-2xl" @click="changePage(currentPage - 1)" :disabled="currentPage === 1">
+      <FontAwesomeIcon :icon="faChevronLeft" />
+    </button>
+
+    <span
+      v-for="page in totalPages"
+      :key="page"
+      @click="changePage(page)"
+      :class="['cursor-pointer', currentPage === page ? 'text-action' : '']"
+    >
+      {{ page }}
+    </span>
+
+    <button class="px-4 py-2 rounded-2xl" @click="changePage(currentPage + 1)" :disabled="currentPage === totalPages">
+      <FontAwesomeIcon :icon="faChevronRight" />
+    </button>
+  </div>
 </template>

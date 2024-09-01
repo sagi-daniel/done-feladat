95aff6d3b94bfd0db77cc15cac6a348311e0d662
dsagi727 <dsagi727@gmail.com>
2024-09-01 11:03:45 +0200

    refact: sidebar menu made resuable component

--- frontend/src/components/layout/Sidebar.vue
+++ frontend/src/components/layout/Sidebar.vue
@@ -1,63 +1,29 @@
 <template>
   <header class="relative">
-    <!-- Hamburger ikon mobilhoz -->
-    <div class="md:hidden flex justify-between items-center p-4 bg-slate-100">
-      <span @click="toggleSidebar" class="text-2xl">
+    <div class="md:hidden flex justify-between items-center p-4">
+      <span @click="openSidebar" class="text-2xl cursor-pointer">
         <font-awesome-icon :icon="faBars" />
       </span>
     </div>
 
-    <!-- Oldalpanel mobilhoz -->
     <div
       :class="[
-        'fixed inset-0 bg-slate-100 text-slate-600 transition-transform transform',
+        'fixed inset-0 transition-transform transform',
         isSidebarOpen ? 'translate-x-0' : '-translate-x-full',
         'md:hidden z-50',
       ]"
     >
-      <!-- Átlátszó háttér -->
       <div v-if="isSidebarOpen" class="fixed inset-0 bg-black opacity-50" @click="closeSidebar"></div>
-      <div class="relative w-3/4 max-w-xs bg-slate-100 h-full p-6">
-        <CloseIcon @onClose="closeSidebar" />
+      <div class="relative w-3/4 max-w-xs h-full bg-primary p-10 flex flex-col gap-20">
+        <CloseIcon @close="closeSidebar" />
         <Logo />
-
-        <!-- Menük -->
-        <nav>
-          <h2 class="text-xs text-black font-bold mb-2">MENÜ</h2>
-          <ul>
-            <li
-              v-for="(menuItem, index) in MENU"
-              :key="index"
-              :class="['flex items-center mb-1 cursor-pointer', { 'text-action': isActive(menuItem) }]"
-            >
-              <router-link :to="menuItem.path || ''" class="flex items-center w-full">
-                <font-awesome-icon :icon="menuItem.icon" class="mr-2" />
-                {{ menuItem.name }}
-              </router-link>
-            </li>
-          </ul>
-        </nav>
+        <Menu :menuItems="MENU" :isActive="isActive" />
       </div>
     </div>
 
-    <!-- Asztali menü -->
-    <div class="hidden md:flex flex-col gap-20 bg-slate-100 h-screen p-10">
+    <div class="hidden md:flex flex-col h-screen p-10 gap-20">
       <Logo />
-      <nav>
-        <h2 class="text-xs text-black font-bold mb-2">MENÜ</h2>
-        <ul>
-          <li
-            v-for="(menuItem, index) in MENU"
-            :key="index"
-            :class="['flex items-center mb-1 cursor-pointer', { 'text-action': isActive(menuItem) }]"
-          >
-            <router-link :to="menuItem.path || ''" class="flex items-center w-full">
-              <font-awesome-icon :icon="menuItem.icon" class="mr-2" />
-              {{ menuItem.name }}
-            </router-link>
-          </li>
-        </ul>
-      </nav>
+      <Menu :menuItems="MENU" :isActive="isActive" />
     </div>
   </header>
 </template>
@@ -68,21 +34,20 @@ import { useRoute } from 'vue-router'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Logo from '../shared/Logo.vue'
+import CloseIcon from '../shared/CloseIcon.vue'
+import Menu from '../shared/Menu.vue'
 import { MENU } from '../../utils/constants'
 
-// Állapotkezelés
 const isSidebarOpen = ref(false)
 
-// Oldalpanel megnyitásának és bezárásának kezelése
-const toggleSidebar = () => {
-  isSidebarOpen.value = !isSidebarOpen.value
+const openSidebar = () => {
+  isSidebarOpen.value = true
 }
 
 const closeSidebar = () => {
   isSidebarOpen.value = false
 }
 
-// Aktív útvonal ellenőrzése
 const route = useRoute()
 const isActive = menuItem => {
   return route.path === menuItem.path
---
+++ frontend/src/components/shared/Menu.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav>
+    <h2 class="text-xs font-bold mb-2">{{ menuTitle }}</h2>
+    <ul>
+      <li
+        v-for="(menuItem, index) in menuItems"
+        :key="index"
+        :class="['flex items-center mb-2 cursor-pointer', { 'text-action': isActive(menuItem) }]"
+      >
+        <router-link :to="menuItem.path || ''" class="flex items-center w-full gap-1">
+          <font-awesome-icon :icon="menuItem.icon" class="mr-2" />
+          {{ menuItem.name }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+const props = defineProps({
+  menuTitle: {
+    type: String,
+    default: 'Menü',
+  },
+  menuItems: Array,
+  isActive: Function,
+})
+</script>

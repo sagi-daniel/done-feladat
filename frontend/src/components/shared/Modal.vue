5f3c2576889517e3aab988ff5eac508ac585c11c
dsagi727 <dsagi727@gmail.com>
2024-09-03 10:47:45 +0200

    refact: removed unuesd watch

--- frontend/src/components/shared/Modal.vue
+++ frontend/src/components/shared/Modal.vue
@@ -22,25 +22,16 @@ const handleBackdropClick = () => {
 const handleModalClick = event => {
   event.stopPropagation()
 }
-
-watch(
-  () => props.isOpen,
-  newValue => {
-    if (!newValue) {
-      // Handle any additional logic
-    }
-  }
-)
 </script>
 
 <template>
   <div
-    v-if="props.isOpen"
-    class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+    v-if="isOpen"
+    class="fixed inset-0 bg-primary bg-opacity-90 flex items-center justify-center z-50"
     @click="handleBackdropClick"
   >
-    <div class="bg-secondary p-6 rounded-lg shadow-lg relative max-w-sm w-full" @click="handleModalClick">
-      <CloseIcon @click="handleBackdropClick" />
+    <div class="bg-secondary text-primary p-6 rounded-lg shadow-lg relative max-w-sm w-full" @click="handleModalClick">
+      <CloseIcon @click="handleBackdropClick" class="absolute top-2 right-2 cursor-pointer" />
       <div class="flex flex-col justify-center items-center px-10 py-2">
         <slot></slot>
       </div>

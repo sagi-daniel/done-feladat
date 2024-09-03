f462b6162219e805388b5078279ebfa3d5247e89
dsagi727 <dsagi727@gmail.com>
2024-09-03 09:28:55 +0200

    feat: moved colors to the config tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,9 +12,20 @@ export default {
       xl: '1440px',
     },
     colors: {
-      primary: 'var(--primary)',
-      secondary: 'var(--light-grey)',
-      action: 'var(--action)',
+      'primary': '#1a1a1a',
+      'secondary': '#f1f1f1',
+      'action': '#f63900',
+      'action-h': '#c32d00',
+      'action-d': '#ff5b2a',
+      'btn-t': '#ffffff',
+
+      'cancel': '#d9d9d9',
+      'text-cancel': '#7e7e7e',
+      'delete': '#f60000',
+      'text-delete': '#ffffff',
+
+      'input-disabled': '#d9d9d9',
+      'input-text-disabled': '#7e7e7e',
     },
     extend: {},
   },

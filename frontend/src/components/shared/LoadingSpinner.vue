3581b63a39394858fd17ee9a286de9ed0e5c7647
dsagi727 <dsagi727@gmail.com>
2024-09-03 10:48:14 +0200

    refact: change style adn add script tag

--- frontend/src/components/shared/LoadingSpinner.vue
+++ frontend/src/components/shared/LoadingSpinner.vue
@@ -1,7 +1,5 @@
-<script setup></script>
-
 <template>
-  <div class="flex justify-center items-center h-screen">
-    <div class="w-12 h-12 border-4 border-action border-t-transparent border-solid rounded-full animate-spin"></div>
+  <div class="flex justify-center items-center h-screen bg-gray-100">
+    <div class="w-16 h-16 border-4 border-action border-t-transparent border-solid animate-spin"></div>
   </div>
 </template>

46ede241e64da9bbdfd939995fe6d9389436057a
dsagi727 <dsagi727@gmail.com>
2024-09-03 09:28:03 +0200

    feat: add fontawesome centralized way

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,24 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faChevronLeft,
+  faChevronRight,
+  faTrash,
+  faPencil,
+  faPlus,
+  faLandmark,
+  faUserGraduate,
+  faBook,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faChevronLeft, faChevronRight, faTrash, faPencil, faPlus, faLandmark, faUserGraduate, faBook)
 
 const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(pinia)
 app.mount('#app')

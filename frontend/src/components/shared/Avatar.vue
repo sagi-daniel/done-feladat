feb4de1e42dd04e521bb390c41664cc5043dbb5e
dsagi727 <dsagi727@gmail.com>
2024-09-07 15:28:21 +0200

    add avatar component

---
+++ frontend/src/components/shared/Avatar.vue
@@ -0,0 +1,32 @@
+<template>
+  <span :class="avatarClasses">
+    {{ userIconWords }}
+  </span>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  userName: String,
+  size: {
+    type: String,
+    default: 'medium',
+    validator: value => ['small', 'medium', 'large'].includes(value),
+  },
+})
+
+const userIconWords = computed(() => props.userName.substring(0, 1).toUpperCase())
+
+const sizeClasses = {
+  small: 'text-base p-2 w-8 h-8',
+  medium: 'text-lg p-3 w-12 h-12',
+  large: 'text-2xl p-5 w-16 h-16',
+}
+
+const avatarClasses = computed(() =>
+  [sizeClasses[props.size], 'font-semibold bg-action font-semibold rounded-full flex items-center justify-center'].join(
+    ' '
+  )
+)
+</script>

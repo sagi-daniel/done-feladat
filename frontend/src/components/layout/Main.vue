cdcd800740b48317a382a93704efededa5e09c81
dsagi727 <dsagi727@gmail.com>
2024-09-07 15:28:11 +0200

    add p-1

--- frontend/src/components/layout/Main.vue
+++ frontend/src/components/layout/Main.vue
@@ -2,7 +2,7 @@
 
 <template>
   <main
-    class="flex w-full flex-col p-1 md:p-10 justify-start items-center text-content rounded-tl-3xl rounded-bl-3xl bg-primary text-secondary"
+    class="flex w-full flex-col p-4 md:p-10 justify-start items-center text-content rounded-tl-3xl rounded-bl-3xl bg-primary text-secondary"
   >
     <slot />
   </main>
